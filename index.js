c0825f0cb591e3aa737d297065a5620b7e2166cf
Darshan <darshankumaravelk@gmail.com>
2024-10-04 21:38:54 +0530

    Created Database Connection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const Database = require('./Config/DatabaseConnection');
 dotenv.config({path:'Config/.env'});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Listening ${process.env.PORT}`);
 });
+Database();
